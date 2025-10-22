b64350dc07f1630e11ca6851025ab09ff80d4da5
Kemily Gomes <kemilygomesoliveira@gmail.com>
2025-10-22 02:34:23 +0000

    camisa

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -6,9 +6,30 @@ class Camisa:
         return self.__tamanho
     
     def setTamanho(self, valor: str):
-        if self.__tamanho == "PP" or "M" or "G" or "GG" or "XG":
-            return True #não sei se está certo. Ajeitar depois.
+        if valor == "PP" or "M" or "G" or "GG" or "XG":
+            self.__tamanho = valor 
         else:
             print("Tamanho inválido. Tamanhos permitidos: PP, P, M, G, GG e XG.")
     
 
+def main():
+    camisa = Camisa()
+    while True:
+        line: str = input()
+        arg: list[str] = line.split(" ")
+        print("$" + line)
+
+        if arg[0] == "end":
+            break
+        elif arg[0] == "grow":
+            camisa(str(arg[1]))
+        elif arg[0] == "":
+            print(self.__camisa())
+        elif arg[0] == "init":
+            camisa : camisa = camisa(arg[1])
+        elif arg[0] == "show":
+            print(camisa)
+        else:
+           print("Comando nao encontrado")
+
+main()
